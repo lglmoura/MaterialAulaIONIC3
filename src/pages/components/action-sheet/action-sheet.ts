8fdd5996eeee38a98ac4eae572c498505fc66165
Luiz Gustavo Lourenco Moura <lglmoura@gmail.com>
2018-11-08 20:28:45 -0200

    alterando ActionSheet

--- src/pages/components/action-sheet/action-sheet.ts
+++ src/pages/components/action-sheet/action-sheet.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ActionSheetController, ActionSheetOptions } from 'ionic-angular';
 
 /**
  * Generated class for the ActionSheetPage page.
@@ -15,9 +15,52 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ActionSheetPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+          public actionSheetCtrl: ActionSheetController) {
   }
 
+  mostraActionSheet = () => {
+
+    let actionSheetOpts : ActionSheetOptions = {
+      title: "Opções",
+      subTitle: "Opções do Usuario",
+      enableBackdropDismiss: false,
+      buttons: [
+        {
+          text: "Excluir",
+          icon:"trash",
+          role: "destructive"
+        },
+        {
+          text: "Cancelar",
+          role: "cancel"
+        },
+        {
+          text: 'Editar',
+          icon:"create",
+          handler: () => {
+            console.log("Editar")
+          }
+        }
+      ]
+    };
+
+    //Spread Operator
+    actionSheetOpts.buttons = [...actionSheetOpts.buttons,{
+      text: "Compartilhar",
+      icon:"share",
+      handler: () => {
+        console.log("Compartilhar");
+        return false;
+      }
+    }]
+
+    const actionSheet = this.actionSheetCtrl.create(actionSheetOpts);
+    actionSheet.present();
+
+    setTimeout(() => actionSheet.dismiss(), 3500);
+
+  }
   ionViewDidLoad() {
     console.log('ionViewDidLoad ActionSheetPage');
   }
