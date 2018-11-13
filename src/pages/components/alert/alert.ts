f59f030f788bfbedf8a8bb5ae343c10c9b70bc92
Luiz Gustavo Lourenco Moura <lglmoura@gmail.com>
2018-11-12 23:45:16 -0200

    Criando e movendo os pipeecia

--- src/pages/components/alert/alert.ts
+++ src/pages/components/alert/alert.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, AlertOptions } from 'ionic-angular';
 
 /**
  * Generated class for the AlertPage page.
@@ -15,11 +15,46 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AlertPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    public alertCtrl: AlertController) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AlertPage');
+  mostrarlertBasico = () => {
+    let alertBasicoOpts : AlertOptions = {
+      title:"Ola aula",
+      subTitle:"Espero que estaja Gostando",
+      message:"Pooa 20182",
+      buttons:["Obrigado"],
+      enableBackdropDismiss:false
+    }
+
+    const alertBasico = this.alertCtrl.create(alertBasicoOpts);
+    alertBasico.present();
+
+
+  }
+
+  mostrarlertPrompt = () => {
+    
+    const alertPrompt = this.alertCtrl.create({
+      inputs:[{
+        name: 'nome',
+        type: "text",
+        placeholder: "Digite o nome",
+        label: "Nome"
+      }],
+      title: "Me diga o seu nome",
+      buttons:[{
+        text: 'Confirmar',
+        handler: data =>{
+          console.log(data);
+          return false;
+        }
+      }]
+    });
+    alertPrompt.present();
+
+
   }
 
 }
