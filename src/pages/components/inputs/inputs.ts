cd2ce6292258a9720ea2109f58c92919f0b2de50
Luiz Gustavo Lourenco Moura <lglmoura@gmail.com>
2018-11-13 19:28:56 -0200

    Refatoramento em Inputs

--- src/pages/components/inputs/inputs.ts
+++ src/pages/components/inputs/inputs.ts
@@ -14,8 +14,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'inputs.html',
 })
 export class InputsPage {
+  public textoArea = "Texto";
   public formI = {
       nome: "",
+      endereco: "",
   };
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
